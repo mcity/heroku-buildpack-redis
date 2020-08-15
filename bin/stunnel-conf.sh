ea45982cf5f50835c48349998322889551bebcdd
Ed Serzo <eserzo@umich.edu>
2020-08-14 20:25:35 -0400

    stunnel

--- bin/stunnel-conf.sh
+++ bin/stunnel-conf.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-URLS=${REDIS_STUNNEL_URLS:-REDIS_URL `compgen -v HEROKU_REDIS`}
+URLS=${REDIS_STUNNEL_URLS:-REDIS_CLOUD_URL}
 n=1
 
 # Enable this option to prevent stunnel from using SSLv3 with cedar-10
 if [ -z "${STUNNEL_FORCE_TLS}" ]; then
   STUNNEL_FORCE_SSL_VERSION=""
 else
-  STUNNEL_FORCE_SSL_VERSION="sslVersion = TLSv1"
+  STUNNEL_FORCE_SSL_VERSION="sslVersion = TLSv1.2"
 fi
 
 mkdir -p /app/vendor/stunnel/var/run/stunnel/
@@ -16,14 +16,16 @@ foreground = yes
 
 pid = /app/vendor/stunnel/stunnel4.pid
 
-options = SINGLE_DH_USE
 socket = r:TCP_NODELAY=1
+socket = l:TCP_NODELAY=1
 options = NO_SSLv3
+TIMEOUTidle = 86400
 ${STUNNEL_FORCE_SSL_VERSION}
 ciphers = HIGH:!ADH:!AECDH:!LOW:!EXP:!MD5:!3DES:!SRP:!PSK:@STRENGTH
+debug = ${STUNNEL_LOGLEVEL:-notice}
 EOFEOF
 
-for URL in $(echo $URLS)
+for URL in $URLS
 do
   eval URL_VALUE=\$$URL
   PARTS=$(echo $URL_VALUE | perl -lne 'print "$1 $2 $3 $4 $5 $6 $7" if /^([^:]+):\/\/([^:]+):([^@]+)@(.*?):(.*?)(\/(.*?)(\\?.*))?$/')
@@ -33,26 +35,15 @@ do
   URI_PASS=${URI[2]}
   URI_HOST=${URI[3]}
   URI_PORT=${URI[4]}
-  URI_PATH=${URI[5]}
-  STUNNEL_PORT=$((URI_PORT + 1))
 
-  STUNNEL_URL=${URI_SCHEME}://$URI_USER:$URI_PASS@127.0.0.1:637${n}
-
-  if [ -z "$URI_PATH" ];
-  then
-    echo "No URI_PATH set"
-  else
-    STUNNEL_URL=${STUNNEL_URL}${URI_PATH}
-  fi
-
-  echo "Setting ${URL} config var with stunnel"
-  export ${URL}=$STUNNEL_URL
+  echo "Setting ${URL}_STUNNEL config var"
+  export ${URL}_STUNNEL=$URI_SCHEME://$URI_USER:$URI_PASS@127.0.0.1:637${n}
 
   cat >> /app/vendor/stunnel/stunnel.conf << EOFEOF
 [$URL]
 client = yes
 accept = 127.0.0.1:637${n}
-connect = $URI_HOST:$STUNNEL_PORT
+connect = $URI_HOST:$URI_PORT
 retry = ${STUNNEL_CONNECTION_RETRY:-"no"}
 EOFEOF
 
